c6e2f4a44f97bff3dac993aec725b255b9c3d306
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2021-04-14 22:08:32 +0200

    Fix question2 tests variable name

--- question2-watercontainer/optimal/function.test.ts
+++ question2-watercontainer/optimal/function.test.ts
@@ -2,32 +2,32 @@ import { maxArea } from './function';
 
 describe('water-container problem optimal solution tests', () => {
     it('should return 0 when the heights array is void ', () => {
-        const height = [];
+        const heights = [];
         const expectedResult = 0;
-        expect(maxArea(height)).toBe(expectedResult);
+        expect(maxArea(heights)).toBe(expectedResult);
     });
 
     it('should return 0 when the heights array contains just one element', () => {
-        const height = [1];
+        const heights = [1];
         const expectedResult = 0;
-        expect(maxArea(height)).toBe(expectedResult);
+        expect(maxArea(heights)).toBe(expectedResult);
     });
 
     it('should return the right value when the heights array contains more than one element', () => {
-        const height = [6, 9, 3, 4, 5, 8];
+        const heights = [6, 9, 3, 4, 5, 8];
         const expectedResult = 32;
-        expect(maxArea(height)).toBe(expectedResult);
+        expect(maxArea(heights)).toBe(expectedResult);
     });
 
     it('should return the right value when the heights array contains more than one element', () => {
-        const height = [1, 7, 9, 2, 1, 6];
+        const heights = [1, 7, 9, 2, 1, 6];
         const expectedResult = 24;
-        expect(maxArea(height)).toBe(expectedResult);
+        expect(maxArea(heights)).toBe(expectedResult);
     });
 
     it('should return the right value when the heights array contains more than one element', () => {
-        const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+        const heights = [1, 8, 6, 2, 5, 4, 8, 3, 7];
         const expectedResult = 49;
-        expect(maxArea(height)).toBe(expectedResult);
+        expect(maxArea(heights)).toBe(expectedResult);
     });
 });

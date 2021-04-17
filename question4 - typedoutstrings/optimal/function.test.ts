baea065fb4d0e4767a7cb9f21e6c53b8b0516df8
delca85 <delca85@gmail.com>
2021-04-17 15:21:11 +0200

    Question4 - compare backspace strings test cases

---
+++ question4 - typedoutstrings/optimal/function.test.ts
@@ -0,0 +1,36 @@
+describe('typed-out-strings problem optimal solution tests', () => {
+    it('should return false when the two strings are different', () => {
+        const s = "b";
+        const t = "a#c";
+        const expectedResult = false;
+        expect(backspaceCompare(s, t)).toBe(expectedResult);
+    });
+
+    it('should return true when the two strings are equal', () => {
+        const s = "a##c";
+        const t = "#a#c";
+        const expectedResult = true;
+        expect(backspaceCompare(s, t)).toBe(expectedResult);
+    });
+
+    it('should return true when the two strings are equal', () => {
+        const s = "ab##";
+        const t = "c#d#";
+        const expectedResult = true;
+        expect(backspaceCompare(s, t)).toBe(expectedResult);
+    });
+
+    it('should return true when the two strings are equal', () => {
+        const s = "ab#c";
+        const t = "ad#c";
+        const expectedResult = true;
+        expect(backspaceCompare(s, t)).toBe(expectedResult);
+    });
+
+    it('should return false when the two strings are equal but in different case', () => {
+        const s = "Ab#c";
+        const t = "ad#c";
+        const expectedResult = false;
+        expect(backspaceCompare(s, t)).toBe(expectedResult);
+    });
+});

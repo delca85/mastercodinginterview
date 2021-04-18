081e30faa1d54514c74a4d361a7b1d45a188c1df
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2021-04-18 15:43:25 +0200

    Question 4 - backspaceCompare optimal solution and pretty bruteforce

--- question4 - typedoutstrings/bruteforce/function.ts
+++ question4 - typedoutstrings/bruteforce/function.ts
@@ -1,3 +1,27 @@
-function backspaceCompare(s: string, t: string): boolean {
-    return false;
-};
+export function backspaceCompare(s: string, t: string): boolean {
+    let sResult = [];
+    let tResult = [];
+    for (let sPointer = 0; sPointer < s.length; sPointer++) {
+        if (s[sPointer] === '#') {
+            sResult.pop();
+        } else {
+            sResult.push(s[sPointer]);
+        }
+    }
+    for (let tPointer = 0; tPointer < t.length; tPointer++) {
+        if (t[tPointer] === '#') {
+            tResult.pop();
+        } else {
+            tResult.push(t[tPointer]);
+        }
+    }
+    if (sResult.length !== tResult.length) {
+        return false;
+    }
+    for (let p = 0; p < sResult.length; p++) {
+        if (sResult[p] !== tResult[p]) {
+            return false;
+        }
+    }
+    return true;
+}
--- question4 - typedoutstrings/optimal/function.ts
+++ question4 - typedoutstrings/optimal/function.ts
@@ -1,3 +1,38 @@
-function backspaceCompare(s: string, t: string): boolean {
-    return false;
-};
+export function backspaceCompare(s: string, t: string): boolean {
+    let sPointer = s.length - 1;
+    let tPointer = t.length - 1;
+
+    while (sPointer >= 0 || tPointer >= 0) {
+        if (s[sPointer] === '#' || t[tPointer] === '#') {
+            if (s[sPointer] === '#') {
+                let sBackcount = 2;
+                while (sBackcount > 0) {
+                    sPointer--;
+                    sBackcount--;
+                    if (s[sPointer] === '#') {
+                        sBackcount += 2;
+                    }
+                }
+            }
+
+            if (t[tPointer] === '#') {
+                let tBackcount = 2;
+                while (tBackcount > 0) {
+                    tPointer--;
+                    tBackcount--;
+                    if (t[tPointer] === '#') {
+                        tBackcount += 2;
+                    }
+                }
+            }
+        } else {
+            if (s[sPointer] !== t[tPointer]) {
+                return false;
+            } else {
+                sPointer--;
+                tPointer--;
+            }
+        }
+    }
+    return true;
+}

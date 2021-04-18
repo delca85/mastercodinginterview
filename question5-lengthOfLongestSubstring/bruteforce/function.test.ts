1e29a6b049b3fb72764f14173f4c757605c83230
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2021-04-18 16:10:58 +0200

    Question 5 - longest substring bruteforce solution

---
+++ question5-lengthOfLongestSubstring/bruteforce/function.test.ts
@@ -0,0 +1,27 @@
+import { lengthOfLongestSubstring } from './function';
+
+describe('lengthOfLongestSubstring - bruteforce solution tests', () => {
+    it('should return 0 when the string is empty', () => {
+        const s = '';
+        const expectedResult = 0;
+        expect(lengthOfLongestSubstring(s)).toBe(expectedResult);
+    });
+
+    it('should return 1 when the string has only 1 char repeated', () => {
+        const s = 'aaaaaaaa';
+        const expectedResult = 1;
+        expect(lengthOfLongestSubstring(s)).toBe(expectedResult);
+    });
+
+    it('should return the length of the longest substring without repetition', () => {
+        const s = 'abccabb';
+        const expectedResult = 3;
+        expect(lengthOfLongestSubstring(s)).toBe(expectedResult);
+    });
+
+    it('should return the length of the longest substring without repetition even if two substrings collide', () => {
+        const s = 'abcbda';
+        const expectedResult = 4;
+        expect(lengthOfLongestSubstring(s)).toBe(expectedResult);
+    });
+});

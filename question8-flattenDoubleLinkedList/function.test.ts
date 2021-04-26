284548281bda8a6b4c4b7c3db97859a1dca1dfb8
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2021-04-26 16:29:36 +0200

    Question 8 - flatten double linked list

--- question8-flattenDoubleLinkedList/function.test.ts
+++ question8-flattenDoubleLinkedList/function.test.ts
@@ -2,9 +2,16 @@ import { flatten } from './function';
 
 describe('Question 8 - Flatten double linked solution tests', () => {
     it('should return the same list if all the nodes are without child', () => {
-        const head = { value: 3, next: { value: 4, next: null } };
-        const expectedResult = { value: 3, next: { value: 4, next: null } };
+        const list = [1, 2, 3, 4, 5, 6];
+        const expectedResult = [1, 2, 3, 4, 5, 6];
 
-        expect(flatten(head)).toEqual(expectedResult);
+        expect(flatten(list)).toEqual(expectedResult);
+    });
+
+    it('should return the a flatten list', () => {
+        const list = [1, 2, 3, [7, 8, [11, 12], 9, 10], 4, 5, 6];
+        const expectedResult = [1, 2, 3, 7, 8, 11, 12, 9, 10, 4, 5, 6];
+
+        expect(flatten(list)).toEqual(expectedResult);
     });
 });
--- question8-flattenDoubleLinkedList/function.ts
+++ question8-flattenDoubleLinkedList/function.ts
@@ -12,36 +12,83 @@ class DoubleLinkedListNode {
     ) {
         this.value = value;
         this.next = next;
-        this.prev = next;
-        this.child = next;
+        this.prev = prev;
+        this.child = child;
     }
 }
 
-type Node = number | number[];
+type Node = number | any[];
 
 function buildMultiLevels(nodes: Node[]): DoubleLinkedListNode {
     const head = new DoubleLinkedListNode(nodes[0] as number);
     let currentNode = head;
 
-    for (let i = 0; i < nodes.length; i++) {
+    for (let i = 1; i < nodes.length; i++) {
         const val = nodes[i];
         let nextNode;
 
         if (Array.isArray(val)) {
-            let child = buildMultiLevels(val);
-            currentNode.child = child;
+            nextNode = buildMultiLevels(val);
+            currentNode.child = nextNode;
             continue;
-        } else {
-            nextNode = new DoubleLinkedListNode(val);
-            currentNode.next = nextNode;
-            nextNode.prev = currentNode;
-            currentNode = nextNode;
         }
+        nextNode = new DoubleLinkedListNode(val);
+        currentNode.next = nextNode;
+        nextNode.prev = currentNode;
+        currentNode = nextNode;
     }
     return head;
 }
 
-export function flatten(nodes: Node[]): DoubleLinkedListNode {
-    const list = buildMultiLevels(nodes);
-    return list;
+function printListMulti(head: DoubleLinkedListNode) {
+    if (head) {
+        console.log(head.value);
+        if (head.child) {
+            printListMulti(head.child);
+        }
+        printListMulti(head.next);
+    }
+}
+
+function toList(head: DoubleLinkedListNode): number[] {
+    let result = [];
+    let currentNode = head;
+    while (currentNode) {
+        result.push(currentNode.value);
+        currentNode = currentNode.next;
+    }
+    return result;
+}
+
+function flattenDoubleLinkedList(
+    head: DoubleLinkedListNode
+): DoubleLinkedListNode {
+    if (!head) return head;
+
+    let currentNode = head;
+    while (currentNode) {
+        if (currentNode.child) {
+            let tail = currentNode.child;
+            while (tail.next) {
+                tail = tail.next;
+            }
+            tail.next = currentNode.next;
+            if (tail.next) {
+                tail.next.prev = tail;
+            }
+            currentNode.next = currentNode.child;
+            currentNode.next.prev = currentNode;
+            currentNode.child = null;
+        } else {
+            currentNode = currentNode.next;
+        }
+    }
+
+    return head;
+}
+
+export function flatten(nodes: Node[]): number[] {
+    const head = buildMultiLevels(nodes);
+    // printListMulti(list);
+    return toList(flattenDoubleLinkedList(head));
 }
